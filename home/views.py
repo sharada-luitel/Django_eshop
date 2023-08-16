83f7b8f27f3bae94eec7dddffa4c329896b6ebbc
sharada-luitel <luitelsharada866@gmail.com>
2023-08-16 23:56:46 +0545

    models

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+class HomeView(View):
+    def get(self,request):
+        return render(request,'index.html')
